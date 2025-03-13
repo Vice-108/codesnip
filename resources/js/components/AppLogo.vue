a2ce2e159291a757d29091f514ba5cf7b4f7762b
vice <vicemark108@gmail.com>
2025-03-14 00:41:48 +0530

    refactor AppLogo.vue for improved layout and alignment

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,10 +3,12 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
-    <div class="flex justify-center items-center bg-sidebar-primary rounded-md size-8 aspect-square text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-6 text-white dark:text-black" />
-    </div>
-    <div class="flex-1 grid ml-1 text-sm text-left">
-        <span class="mb-0.5 font-semibold text-lg truncate leading-none">CodeSnip</span>
+    <div class="flex items-center">
+        <div class="flex justify-center items-center bg-sidebar-primary rounded-md size-8 aspect-square text-sidebar-primary-foreground">
+            <AppLogoIcon class="size-6 text-white dark:text-black" />
+        </div>
+        <div class="flex flex-1 items-center ml-1">
+            <span class="font-semibold text-lg truncate leading-none">CodeSnip</span>
+        </div>
     </div>
 </template>
